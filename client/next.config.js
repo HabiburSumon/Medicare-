7531be4165dee62437952f62693ce538c1176008
HabiburSumon <mdhabiburrahmansumon85@gmail.com>
2026-05-10 12:28:19 +0600

    fix: add localhost to allowed image hosts in next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '**' },
+      { protocol: 'http', hostname: 'localhost' },
     ],
   },
   async headers() {
